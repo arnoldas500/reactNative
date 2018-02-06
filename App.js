2689e49def8c4730da93d15d25b2bcaeb7e2a720
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2018-02-05 22:31:18 -0500

    init commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Alert, Image } from 'react-native';
+import { Constants, BarCodeScanner, Permissions } from 'expo';
+
+export default class App extends Component {
+  state = {
+    hasCameraPermission: null
+  };
+
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = data => {
+    Alert.alert(
+      'Scan successful!',
+      JSON.stringify(data)
+    );
+  };
+
+  render() {
+      return (
+	  
+
+	      
+	      <View style={styles.container}>
+	      <Image source={require('./assets/Arrow.jpg')} />
+	      <Text>Welcome to Arrow Security Barcode app!{"\n"}Scan barcode to see barcode info!</Text>
+        {this.state.hasCameraPermission === null ?
+          <Text>Requesting for camera permission</Text> :
+          this.state.hasCameraPermission === false ?
+            <Text>Camera permission is not granted</Text> :
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={{ height: 200, width: 200 }}
+            />
+        }
+	  </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  }
+});
